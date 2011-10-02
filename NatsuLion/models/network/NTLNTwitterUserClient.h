8b0aba01a79c5b3fdcb144806e000a2d9bbf4e6a
Austin Treat Emmons <atreatemmons@gmail.com>
2011-10-02 23:49:33 +0100

    Looked into CoreLocation and <CLLocationManagerDelegate>

--- NatsuLion/models/network/NTLNTwitterUserClient.h
+++ NatsuLion/models/network/NTLNTwitterUserClient.h
@@ -3,6 +3,8 @@
 #import "NTLNHttpClient.h"
 #import "NTLNOAuthHttpClient.h"
 
+#import <CoreLocation/CoreLocation.h>
+
 @class NTLNTwitterUserClient;
 
 @protocol NTLNTwitterUserClientDelegate
@@ -13,20 +15,45 @@
 #ifdef ENABLE_OAUTH
 @interface NTLNTwitterUserClient : NTLNOAuthHttpClient {
 #else
-@interface NTLNTwitterUserClient : NTLNHttpClient {
+@interface NTLNTwitterUserClient : NTLNHttpClient <CLLocationManagerDelegate> {
 #endif
-@private
-	NSObject<NTLNTwitterUserClientDelegate> *delegate;
-	NSMutableArray *users;
-}
+    @private
+        NSObject<NTLNTwitterUserClientDelegate> *delegate;
+    //    iOS Dev
+//        NSObject<CLLocationManagerDelegate> *_MYLocationDelegate; //delegate defined for self on @interface
+        CLLocationManager *locationManager;
+        
+        
+        
+        
+        
+        
+        NSMutableArray *users;
+    }
 
-@property (readwrite, retain) NSObject<NTLNTwitterUserClientDelegate> *delegate;
+    @property (readwrite, retain) NSObject<NTLNTwitterUserClientDelegate> *delegate;
+//    iOS Dev
+//    @property (readwrite, retain) NSObject<CLLocationManagerDelegate> *_MYLocationDelegate;  //see @synthesize call in implementation
+    @property (readwrite, retain) CLLocationManager *locationManager;
+        
+        
 
-- (void)getUserInfoForScreenName:(NSString*)screen_name;
-- (void)getUserInfoForUserId:(NSString*)user_id;
-- (void)getFollowingsWithScreenName:(NSString*)screen_name page:(int)page;
-- (void)getFollowersWithScreenName:(NSString*)screen_name page:(int)page;
+    #pragma mark -
+    #pragma mark iOS Dev
+    //    iOs Dev
+    //
+    //    
+    - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation;    
+    - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error;
+    //- (void)getUserLocationWithGPS;
+        
+        
+    //
+    - (void)getUserInfoForScreenName:(NSString*)screen_name;
+    - (void)getUserInfoForUserId:(NSString*)user_id;
+    - (void)getFollowingsWithScreenName:(NSString*)screen_name page:(int)page;
+    - (void)getFollowersWithScreenName:(NSString*)screen_name page:(int)page;
 
-@property (readonly) NSMutableArray *users;
+    @property (readonly) NSMutableArray *users;
 
 @end
--- NatsuLion/models/network/NTLNTwitterUserClient.m
+++ NatsuLion/models/network/NTLNTwitterUserClient.m
@@ -3,6 +3,8 @@
 #import "NTLNAccount.h"
 #import "NTLNHttpClientPool.h"
 
+
+
 @implementation NTLNTwitterUserClient
 
 @synthesize delegate;
@@ -14,6 +16,60 @@
 	[super dealloc];
 }
 
+#pragma mark -
+#pragma mark iOS Dev Team
+//
+//. All iOS Dev code added here
+//
+//.     Want to call function from MYFLLocalTweetViewController
+//.     Will grab location using CoreLocation
+//.     Notice #import of <CoreLocation/CoreLocation.h> in header file
+//
+//.     Reading tutorial here http://mobileorchard.com/hello-there-a-corelocation-tutorial/
+//.     Added instance variable of delegate, MYLocationDelegate instead of calling self.
+//
+
+//@synthesize _MYLocationDelegate = MYLocationDelegate; 
+//.     Looks like clashing variable names with <CLLocationManagerDelegate> and <NTLNTwitterUserClientDelegate>
+
+@synthesize locationManager;
+
+//.     init only necessary when self is the delegate. Try to fix by using instance variable method as used with <NTLNTwitterUserClientDelegate>
+//
+//- (id) init {
+//    self = [super init];
+//    if (self != nil) {
+//        self.locationManager = [[[CLLocationManager alloc] init] autorelease];
+//        self.locationManager.delegate = self; // send loc updates to myself
+//    }
+//    return self;
+//}
+
+
+
+
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    
+}
+
+- (void)getUserLocationWithGPS{
+        
+
+}
+
+
+
+
+
+
+#pragma mark -
+#pragma mark User Info
 - (void)getUserInfo:(NSString*)q {
 	NSString *url = [NSString stringWithFormat:@"http://twitter.com/users/show/%@.xml", q];
 	[super requestGET:url];
